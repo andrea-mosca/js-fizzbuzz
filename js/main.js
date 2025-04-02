c9e892477fbc4d9afba354907145a045899f76d9
andrea-mosca <andrea.moscateli@gmail.com>
2025-04-02 17:21:42 +0200

    short solution

--- js/main.js
+++ js/main.js
@@ -9,20 +9,17 @@
 
 // stampa i numeri da 1 a 100
 for (i = 1; i <= 100; i++){
-    // se il numero è divisibile per 5 e per 3, stampa FizzBuzz 
-    if (i % 5 === 0 && i % 3 === 0){
-        console.log("FizzBuzz")
-    } 
-    // se il numero è divisibile per 5, ma non per 3, stampa Buzz
-    else if (i % 5 === 0){
-        console.log("Buzz")
-    } 
-    // se il numero è divisibile per 3, ma non per 5, stampa Fizz
-    else if (i % 3 === 0){
-        console.log("Fizz")
+    if (i % 5 === 0 && i % 3 === 0){// se il numero è divisibile per 5 e per 3, stampa FizzBuzz
+        console.log("FizzBuzz");
+    } else if (i % 5 === 0){ // se il numero è divisibile per 5, ma non per 3, stampa Buzz
+        console.log("Buzz");
+    } else if (i % 3 === 0){// se il numero è divisibile per 3, ma non per 5, stampa Fizz
+        console.log("Fizz");
+    } else{// se non è divisibile per 3 o 5, stampa il numero
+        console.log(i);
     }
-    // se non è divisibile per 3 o 5, stampa il numero
-    else{
-        console.log(i)
-    }
+}
+
+for (i = 1; i <= 100; i++){
+    console.log( (i %3 ? "" : "Fizz") +(i % 5 ? "" : "Buzz") || i);
 }
