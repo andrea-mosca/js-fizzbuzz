b1ba797d4f7d07b1bcbe6f6710486a3e0190b715
andrea-mosca <andrea.moscateli@gmail.com>
2025-04-02 13:54:54 +0200

    commenti

--- js/main.js
+++ js/main.js
@@ -6,14 +6,23 @@
 // - per i multipli di 3 e di 5 stampi "FizzBuzz" al posto del numero
 
 // SVOLGIMENTO 
+
+// stampa i numeri da 1 a 100
 for (i = 1; i <= 100; i++){
+    // se il numero è divisibile per 5 e per 3, stampa FizzBuzz 
     if (i % 5 === 0 && i % 3 === 0){
         console.log("FizzBuzz")
-    } else if (i % 5 === 0){
+    } 
+    // se il numero è divisibile per 5, ma non per 3, stampa Buzz
+    else if (i % 5 === 0){
         console.log("Buzz")
-    } else if (i % 3 === 0){
+    } 
+    // se il numero è divisibile per 3, ma non per 5, stampa Fizz
+    else if (i % 3 === 0){
         console.log("Fizz")
-    } else{
+    }
+    // se non è divisibile per 3 o 5, stampa il numero
+    else{
         console.log(i)
     }
 }
