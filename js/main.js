3b166669faebed813c5c940572cfe843b24e1ec6
andrea-mosca <andrea.moscateli@gmail.com>
2025-04-02 13:50:31 +0200

    script

---
+++ js/main.js
@@ -0,0 +1,19 @@
+//----- TRACCIA -----
+
+// Scrivi un programma che stampi i numeri da 1 a 100,
+// - per i multipli di 3 stampi "Fizz" al posto del numero
+// - per i multipli di 5 stampi "Buzz" al posto del numero
+// - per i multipli di 3 e di 5 stampi "FizzBuzz" al posto del numero
+
+// SVOLGIMENTO 
+for (i = 1; i <= 100; i++){
+    if (i % 5 === 0 && i % 3 === 0){
+        console.log("FizzBuzz")
+    } else if (i % 5 === 0){
+        console.log("Buzz")
+    } else if (i % 3 === 0){
+        console.log("Fizz")
+    } else{
+        console.log(i)
+    }
+}
